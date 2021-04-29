104078cb6b73e977bea24edc91fa8d1527e751a2
flipascot <55513932+flipascot@users.noreply.github.com>
2021-04-29 09:49:50 +0200

    Update drill16.cpp

--- drill16/drill16.cpp
+++ drill16/drill16.cpp
@@ -1,80 +1,157 @@
+/* 
+g++ -w -Wall -std=c++11 ../GUI/Graph.cpp ../GUI/Window.cpp ../GUI/GUI.cpp ../GUI/Simple_window.cpp ch16_drill.cpp `fltk-config --ldflags --use-images` -o a.out
+*/
 #include "GUI.h"
-#include "std_lib_facilities.h"
-#include <sstream>
+
+#include <iostream>
 
 using namespace Graph_lib;
 
-void Button::attach(Window& win)
+struct Lines_window : Graph_lib::Window {
+    Lines_window(Point xy, int w, int h, const string& title);
+private:
+    
+    Open_polyline lines;
+
+   
+    Button next_button;     
+    Button quit_button;     
+    In_box next_x;
+    In_box next_y;
+    Out_box xy_out;
+    Menu color_menu;
+    Button color_button;
+    Menu style_menu;
+    Button style_button;
+
+    void change_c(Color c) { lines.set_color(c); }
+    void change_s(Line_style s) { lines.set_style(s); }
+
+    void hide_color() { color_menu.hide(); color_button.show(); }
+    void hide_style() { style_menu.hide(); style_button.show(); }
+
+    
+    void red_pressed() { change_c(Color::red); hide_color(); }
+    void blue_pressed() { change_c(Color::blue); hide_color(); }
+    void black_pressed() { change_c(Color::black); hide_color(); }
+
+    void dot_pressed() { change_s(Line_style::dot); hide_style(); }
+    void dash_pressed() { change_s(Line_style::dash); hide_style(); }
+    void solid_pressed() { change_s(Line_style::solid); hide_style(); }
+
+    void color_pressed() { color_button.hide(); color_menu.show(); }
+    void style_pressed() { style_button.hide(); style_menu.show(); }
+    void next();
+    void quit();
+
+    
+    /* visszahícós functiom
+    static void cb_red(Address, Address);
+    static void cb_blue(Address, Address);
+    static void cb_black(Address, Address);
+    static void cb_menu(Address, Address);
+    static void cb_next(Address, Address);
+    static void cb_quit(Address, Address);
+    */
+};
+
+Lines_window::Lines_window(Point xy, int w, int h, const string& title)
+    : Window{xy, w, h, title},
+    next_button{Point{x_max() - 150, 0}, 70, 20, "Next point",
+        [](Address, Address pw) { reference_to<Lines_window>(pw).next(); }},
+    quit_button{Point{x_max() - 70, 0}, 70, 20, "Quit",
+        [](Address, Address pw) { reference_to<Lines_window>(pw).quit(); }},
+    next_x{Point{x_max() - 310, 0}, 50, 20, "next x:"},
+    next_y{Point{x_max() - 210, 0}, 50, 20, "next y:"},
+    xy_out{Point{100, 0}, 100, 20, "current (x,y):"},
+    color_menu{Point{x_max() - 70, 30}, 70, 20, Menu::vertical, "color"},
+    color_button{Point{x_max() - 80, 30}, 80, 20, "color menu",
+        [](Address, Address pw) {
+            reference_to<Lines_window>(pw).color_pressed();
+        }
+    },
+    style_menu{Point{10, 30}, 70, 20, Menu::vertical, "style"},
+    style_button{Point{0, 30}, 80, 20, "style menu",
+        [](Address, Address pw) {
+            reference_to<Lines_window>(pw).style_pressed();
+        }
+    }
 {
-	pw = new Fl_Button(loc.x, loc.y, width, height, label.c_str());
-	pw->callback(reinterpret_cast<Fl_Callback*>(do_it), &win);
-	own = &win;
+    attach(next_button);
+    attach(quit_button);
+    attach(next_x);
+    attach(next_y);
+    attach(xy_out);
+    xy_out.put("no point");
+    color_menu.attach(new Button{Point{0, 0}, 0, 0, "red",
+        [](Address, Address pw) {
+            reference_to<Lines_window>(pw).red_pressed();
+        }
+    });
+    color_menu.attach(new Button{Point{0, 0}, 0, 0, "blue",
+        [](Address, Address pw) {
+            reference_to<Lines_window>(pw).blue_pressed();
+        }
+    });
+    color_menu.attach(new Button{Point{0, 0}, 0, 0, "black",
+        [](Address, Address pw) {
+            reference_to<Lines_window>(pw).black_pressed();
+        }
+    });
+    style_menu.attach(new Button{Point{0, 0}, 0, 0, "dot",
+        [](Address, Address pw) {
+            reference_to<Lines_window>(pw).dot_pressed();
+        }
+    });
+    style_menu.attach(new Button{Point{0, 0}, 0, 0, "dash",
+        [](Address, Address pw) {
+            reference_to<Lines_window>(pw).dash_pressed();
+        }
+    });
+    style_menu.attach(new Button{Point{0, 0}, 0, 0, "solid",
+        [](Address, Address pw) {
+            reference_to<Lines_window>(pw).solid_pressed();
+        }
+    });
+    attach(color_menu);
+    attach(style_menu);
+    color_menu.hide();
+    style_menu.hide();
+    attach(color_button);
+    attach(style_button);
+    attach(lines);
 }
 
-int In_box::get_int()
+void Lines_window::quit()
 {
-	Fl_Input& pi = reference_to<Fl_Input>(pw);
-	const char* p = pi.value();
-	if (!isdigit(p[0])) return -999999;
-	return atoi(p);
+    hide();     
 }
 
-string In_box::get_string()
+void Lines_window::next()
 {
-	Fl_Input& pi = reference_to<Fl_Input>(pw);
-	return string(pi.value());
+    int x = next_x.get_int();
+    int y = next_y.get_int();
+
+    lines.add(Point{x, y});
+
+   
+    ostringstream ss;
+    ss << '(' << x << ',' << y << ')';
+    xy_out.put(ss.str());
+
+    redraw();
 }
 
-void In_box::attach(Window& win)
-{
-	pw = new Fl_Input(loc.x, loc.y, width, height, label.c_str());
-	own = &win;
+int main()
+try {
+    Lines_window win {Point{100, 100}, 600, 400, "lines"};
+    return gui_main();
 }
-
-void Out_box::put(int i)
-{
-	Fl_Output& po = reference_to<Fl_Output>(pw);
-	std::stringstream ss;
-	ss << i;
-	po.value(ss.str().c_str());
+catch(exception& e) {
+    cerr << "Error: " << e.what() << '\n';
+    return 1;
 }
-
-void Out_box::put(const string& s)
-{
-	reference_to<Fl_Output>(pw).value(s.c_str());
-}
-
-void Out_box::attach(Window& win)
-{
-	pw = new Fl_Output(loc.x, loc.y, width, height, label.c_str());
-	own = &win;
-}
-
-Menu::Menu(Point xy, int w, int h, Kind kk, const string& s)
-:Widget(xy,w,h,s,0), k(kk), offset(0)
-{
-}
-
-int Menu::attach(Button& b)
-{
-	b.width = width;
-	b.height = height;
-
-	switch(k) {
-	case horizontal:
-		b.loc = Point(loc.x+offset,loc.y);
-		offset+=b.width;
-		break;
-	case vertical:
-		b.loc = Point(loc.x,loc.y+offset);
-		offset+=b.height;
-		break;
-	}
-	selection.push_back(&b);
-	return int(selection.size()-1);
-}
-
-int Menu::attach(Button* p)
-{
-	return attach(*p);
+catch(...) {
+    cerr << "Unknown exception\n";
+    return 2;
 }
